5ea9b2514ebe83398525ca081d23698e09ad4ecd
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-07-25 10:01:44 +0800

    切片程序退出后，延迟3秒后再返回，确保其他流程已完成

--- segment/SegmentService.js
+++ segment/SegmentService.js
@@ -54,7 +54,7 @@ export default class SegmentService {
                     });
                     p.on('exit', (code) => {
                         console.log(`切片程序退出:${filename}, code:${code}`);
-                        setTimeout(() => res(), 3000);
+                        res();
                     });
                     p.on('error', (error) => {
                         console.log(error);
