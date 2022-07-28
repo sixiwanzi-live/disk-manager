1ff467f0e262e187b9ee1442f89dcb8016d2d569
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-07-28 17:11:48 +0800

    增加音频切片

--- segment/SegmentService.js
+++ segment/SegmentService.js
@@ -13,7 +13,8 @@ export default class SegmentService {
         const bv        = ctx.request.query.bv;
         const startTime = ctx.request.query.startTime;
         const endTime   = ctx.request.query.endTime;
-        console.log(`req:${bv}, ${startTime}, ${endTime}`);
+        const audio     = ctx.request.query.audio || false;
+        console.log(`req:${bv}, ${startTime}, ${endTime}, ${audio}`);
         // 检查bv是否合法
         if (!bv || bv.length !== 12) {
             throw error.disk.BvIllegal;
@@ -40,13 +41,18 @@ export default class SegmentService {
             throw error.segment.ResourceNotFound;
         }
 
-        const filename = `${bv}-${toTime(startTime).replaceAll(':', '-')}--${toTime(endTime).replaceAll(':', '-')}.mp4`;
+        let filename = `${bv}-${toTime(startTime).replaceAll(':', '-')}--${toTime(endTime).replaceAll(':', '-')}`;
+        filename = audio ? filename + '.aac' : filename + '.mp4'; 
+        // const filename = `${bv}-${toTime(startTime).replaceAll(':', '-')}--${toTime(endTime).replaceAll(':', '-')}.mp4`;
         const output = `${config.disk.path}/segment/${filename}`;
         try {
             await stat(output); // 检查output是否存在，避免重复生成
         } catch (ex) {
             // const cmd = `ffmpeg -i "${resource}" -ss ${toTime(startTime)} -to ${toTime(endTime)} -c copy "${output}"`;
-            const cmd = `ffmpeg -ss ${toTime(startTime)} -to ${toTime(endTime)} -accurate_seek -i "${resource}" -c copy -avoid_negative_ts 1 "${output}"`;
+            const cmd = audio ? 
+                            `ffmpeg -vn -ss ${toTime(startTime)} -to ${toTime(endTime)} -accurate_seek -i "${resource}" -c copy -avoid_negative_ts 1 "${output}"` :
+                            `ffmpeg -ss ${toTime(startTime)} -to ${toTime(endTime)} -accurate_seek -i "${resource}" -c copy -avoid_negative_ts 1 "${output}"`;
+            // const cmd = `ffmpeg -ss ${toTime(startTime)} -to ${toTime(endTime)} -accurate_seek -i "${resource}" -c copy -avoid_negative_ts 1 "${output}"`;
             console.log(cmd);
             try {
                 await new Promise((res, rej) => {
