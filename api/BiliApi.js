7de83b00b64a63957edd44d05dcd17141db5f91e
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-08-14 19:44:42 +0800

    封装b站api

---
+++ api/BiliApi.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import error from '../error.js';
+import config from '../config.js';
+
+export default class BiliApi {
+    static async fetchCid(bv) {
+        try {
+            const res = await axios.get(`https://api.bilibili.com/x/web-interface/view?bvid=${bv}`);
+            return res.data.data.cid;
+        } catch (ex) {
+            console.log(ex.response.data);
+            throw error.segment.CidNotFound;
+        }
+    }
+
+    static async fetchStreamUrl(bv, cid, qn) {
+        const playurl = `https://api.bilibili.com/x/player/playurl?bvid=${bv}&cid=${cid}&qn=${qn}&fourk=1`;
+        console.log(playurl);
+        let url = "";
+        while (true) {
+            try {
+                const res = await axios.get(playurl, {
+                    headers: {
+                        "Cache-Control": "no-cache",
+                        "Accept-Encoding": "gzip, deflate, br",
+                        "User-Agent": config.segment.userAgent,
+                        "Cookie": `SESSDATA=${config.segment.sessdata};`
+                    },
+                });
+                url = res.data.data.durl[0].url;
+            } catch (ex) {
+                console.log(ex.response.data);
+                throw error.segment.StreamNotFound;
+            }
+            // 如果读取到的流是mcdn开头的，该流下载速度极慢，需要重新读取
+            if (url.indexOf('mcdn') === -1) {
+                break;
+            }
+        }
+        return url;
+    }
+}
--- segment/SegmentService.js
+++ segment/SegmentService.js
@@ -1,9 +1,9 @@
 import { unlink } from 'fs/promises';
 import {spawn} from 'child_process';
-import axios from "axios";
 import error from "../error.js";
 import config from '../config.js';
 import {toTime} from '../util.js';
+import BiliApi from '../api/BiliApi.js';
 import PushApi from '../api/PushApi.js';
 
 export default class SegmentService {
@@ -35,44 +35,9 @@ export default class SegmentService {
             console.log(ex);
         }
 
-        let cid = '';
-        try {
-            const res = await axios.get(`https://api.bilibili.com/x/web-interface/view?bvid=${bv}`);
-            cid = res.data.data.cid;
-        } catch (ex) {
-            console.log(ex.response.data);
-            throw error.segment.CidNotFound;
-        }
-
-        let qn = 120;
-        if (audio === 'true') {
-            // 如果只是为了下载音频，没必要下载高清视频切片
-            qn = 64;
-        }
-        const playurl = `https://api.bilibili.com/x/player/playurl?bvid=${bv}&cid=${cid}&qn=${qn}&fourk=1`;
-        console.log(playurl);
-        let url = "";
-        while (true) {
-            try {
-                const res = await axios.get(playurl, {
-                    headers: {
-                        "Cache-Control": "no-cache",
-                        "Accept-Encoding": "gzip, deflate, br",
-                        "User-Agent": config.segment.userAgent,
-                        "Cookie": `SESSDATA=${config.segment.sessdata};`
-                    },
-                });
-                url = res.data.data.durl[0].url;
-            } catch (ex) {
-                console.log(ex.response.data);
-                throw error.segment.StreamNotFound;
-            }
-            // 如果读取到的流是mcdn开头的，该流下载速度极慢，需要重新读取
-            if (url.indexOf('mcdn') === -1) {
-                break;
-            }
-        }
-        console.log(url);
+        const cid = await BiliApi.fetchCid(bv);
+        const streamUrl = await BiliApi.fetchStreamUrl(bv, cid, audio === 'true' ? 64 : 120);
+        console.log(streamUrl);
         
         try {
             await new Promise((res, rej) => {
@@ -84,7 +49,7 @@ export default class SegmentService {
                     '-seekable', 1, 
                     '-user_agent', config.segment.userAgent, 
                     '-headers', `Referer: ${config.segment.referer}`,
-                    '-i', url,
+                    '-i', streamUrl,
                     '-c', 'copy',
                     '-avoid_negative_ts', 1,
                     videoOutput,
