0d7056644ae0f5a6a5d1dd97ece18365164f3033
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-08-24 22:16:41 +0800

    生成切片前，先看看本地缓存是否有源视频

--- segment/SegmentService.js
+++ segment/SegmentService.js
@@ -49,7 +49,6 @@ export default class SegmentService {
                 '-ss', toTime(startTime), 
                 '-to', toTime(endTime), 
                 '-accurate_seek', 
-                '-seekable', 1, 
                 '-i', src,
                 '-c', 'copy',
                 '-avoid_negative_ts', 1,
