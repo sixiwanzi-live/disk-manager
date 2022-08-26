39edc2032028a42662ff95f7e8577fb94d127055
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-08-26 17:34:10 +0800

    不再缓存视频，而缓存B站的视频源url;暂时重新使用sessdata

--- segment/SegmentService.js
+++ segment/SegmentService.js
@@ -11,59 +11,11 @@ import ZimuApi from '../api/ZimuApi.js';
 export default class SegmentService {
 
     constructor() {
-        this.emitter = new EventEmitter();
-        this.emitter.on('cache', async (clipId, url) => {
-            const output = `${config.disk.path}/video/${clipId}.mp4`;
-            try {
-                await stat(output);
-            } catch (ex) {
-                try {
-                    await new Promise((res, rej) => {
-                        let cmd = [
-                            '-user_agent', config.segment.userAgent, 
-                            '-headers', `Referer: ${config.segment.referer}`,
-                            '-i', url,
-                            '-c', 'copy',
-                            output,
-                            '-v', 'debug'
-                        ];
-                        let p = spawn('ffmpeg', cmd);
-                        p.stdout.on('data', (data) => {
-                            console.log('stdout: ' + data.toString());
-                        });
-                        p.stderr.on('data', (data) => {
-                            console.log('stderr: ' + data.toString());
-                        });
-                        p.on('close', (code) => {
-                            console.log(`下载程序退出:${clipId}, code:${code}`);
-                            res();
-                        });
-                        p.on('error', (error) => {
-                            console.log(error);
-                            rej(error);
-                        });
-                    });
-                    try {
-                        await stat(output);
-                        await PushApi.push('视频下载完成', `${clipId}.mp4`);
-                    } catch (ex2) {
-                        console.log(ex2);
-                        await PushApi.push('找不到下载视频', `${clipId}.mp4`);
-                        throw ex2;
-                    }
-                } catch (ex) {
-                    console.log(ex);
-                    throw {
-                        code: error.disk.DownloadFailed.code,
-                        message: ex
-                    }
-                }
-            }
-        });
+        this.srcMap = new Map();
     }
 
     make = async (ctx) => {
-        const clipId    = ctx.params.clipId;
+        const clipId    = parseInt(ctx.params.clipId);
         const startTime = ctx.request.query.startTime;
         const endTime   = ctx.request.query.endTime;
         const audio     = ctx.request.query.audio || 'false';
@@ -92,24 +44,52 @@ export default class SegmentService {
         const clip = await ZimuApi.findClipById(clipId);
         console.log(clip);
 
-        try {
-            // 先尝试使用本地源生成切片
-            const src = `${config.disk.path}/video/${clipId}.mp4`;
-            const res = await stat(src);
-            console.log(`源视频地址:${src}`);
-            let cmd = [
+        // 获取视频源地址并生成ffmpeg参数，B站视频需要先查缓存，如果缓存没有，需要请求B站获取
+        // 非B站视频直接使用playUrl
+        let src = '';
+        let cmd = [];
+        if (clip.type === 1) {
+            if (this.srcMap.has(clipId)) {
+                src = this.srcMap.get(clipId);
+                console.log(`clip(${clipId})命中缓存`);
+            } else {
+                console.log(`clip(${clipId})未命中缓存`);
+            }
+            cmd = [
                 '-ss', toTime(startTime), 
                 '-to', toTime(endTime), 
                 '-accurate_seek', 
+                '-seekable', 1, 
+                '-user_agent', config.segment.userAgent, 
+                '-headers', `Referer: ${config.segment.referer}`,
                 '-i', src,
                 '-c', 'copy',
                 '-avoid_negative_ts', 1,
-                output,
-                '-v', 'debug'
+                output
             ];
+            // 仅下载音频需要添加额外的命令行参数
             if (audio === 'true') {
                 cmd = ['-vn', ...cmd];
             }
+        } else {
+            src = `https://${clip.playUrl}`;
+            cmd = [
+                '-ss', toTime(startTime), 
+                '-to', toTime(endTime), 
+                '-accurate_seek', 
+                '-seekable', 1, 
+                '-i', src,
+                '-c', 'copy',
+                '-avoid_negative_ts', 1,
+                output
+            ];
+        }
+        // 仅下载音频需要添加额外的命令行参数
+        if (audio === 'true') {
+            cmd = ['-vn', ...cmd];
+        }
+
+        try {
             await new Promise((res, rej) => {
                 let p = spawn('ffmpeg', cmd);
                 p.stdout.on('data', (data) => {
@@ -129,58 +109,76 @@ export default class SegmentService {
             });
             await stat(output);
         } catch (ex) {
-            // 如果本地源生成切片失败，则使用远程源
-            let src = '';
+            console.log(ex);
             if (clip.type === 1) {
-                // 获取B站视频源
-                // playUrl的格式为player.bilibili.com/player.html?bvid=BV1Mg411C7FP,所以取后12个字符为bv
+                console.log(`clip(${clipId})缓存可能失效，需重新获取`);
+                // 在下载切片失败的情况下，如果是从B站下载失败的，说明视频源有问题，需要重新申请视频源
+                if (this.srcMap.has(clipId)) {
+                    this.srcMap.delete(clipId);
+                }
+                // 重新获取bv和cid
                 const bv = clip.playUrl.substring(clip.playUrl.length - 12);
                 const cid = await BiliApi.fetchCid(bv);
-                src = await BiliApi.fetchStreamUrl(bv, cid, audio === 'true' ? 80: 120);
+                try {
+                    // 重新获取视频流
+                    const qn = 120;
+                    src = await BiliApi.fetchStreamUrl(bv, cid, qn);
+                    // 将获取到的新src保存起来
+                    this.srcMap.set(clipId, src);
+                } catch (ex) {
+                    console.log(ex.response.data);
+                    throw error.segment.StreamNotFound;
+                }
+                // 重新生成ffmepg命令行参数
+                cmd = [
+                    '-ss', toTime(startTime), 
+                    '-to', toTime(endTime), 
+                    '-accurate_seek', 
+                    '-seekable', 1, 
+                    '-user_agent', config.segment.userAgent, 
+                    '-headers', `Referer: ${config.segment.referer}`,
+                    '-i', src,
+                    '-c', 'copy',
+                    '-avoid_negative_ts', 1,
+                    output
+                ];
+                // 仅下载音频需要添加额外的命令行参数
+                if (audio === 'true') {
+                    cmd = ['-vn', ...cmd];
+                }
+                try {
+                    await new Promise((res, rej) => {
+                        let p = spawn('ffmpeg', cmd);
+                        p.stdout.on('data', (data) => {
+                            console.log('stdout: ' + data.toString());
+                        });
+                        p.stderr.on('data', (data) => {
+                            console.log('stderr: ' + data.toString());
+                        });
+                        p.on('close', (code) => {
+                            console.log(`ffmpeg退出:${clip.id}-${clip.title}, code:${code}`);
+                            res();
+                        });
+                        p.on('error', (error) => {
+                            console.log(error);
+                            rej(error);
+                        });
+                    });
+                } catch (ex) {
+                    console.log(ex);
+                    throw error.segment.Failed;
+                }
             } else {
-                src = `https://${clip.playUrl}`;
-            }
-            console.log(`源视频地址:${src}`);
-            this.emitter.emit('cache', clipId, src);
-            let cmd = [
-                '-ss', toTime(startTime), 
-                '-to', toTime(endTime), 
-                '-accurate_seek', 
-                '-seekable', 1, 
-                '-user_agent', config.segment.userAgent, 
-                '-headers', `Referer: ${config.segment.referer}`,
-                '-i', src,
-                '-c', 'copy',
-                '-avoid_negative_ts', 1,
-                output,
-                '-v', 'debug'
-            ];
-            if (audio === 'true') {
-                cmd = ['-vn', ...cmd];
-            }
-            try {
-                await new Promise((res, rej) => {
-                    let p = spawn('ffmpeg', cmd);
-                    p.stdout.on('data', (data) => {
-                        console.log('stdout: ' + data.toString());
-                    });
-                    p.stderr.on('data', (data) => {
-                        console.log('stderr: ' + data.toString());
-                    });
-                    p.on('close', (code) => {
-                        console.log(`ffmpeg退出:${clip.id}-${clip.title}, code:${code}`);
-                        res();
-                    });
-                    p.on('error', (error) => {
-                        console.log(error);
-                        rej(error);
-                    });
-                });
-                await stat(output);
-            } catch (ex) {
-                console.log(ex);
+                // 在下载切片失败的情况下，如果是从录播站下载失败的，则直接报错。
                 throw error.segment.Failed;
             }
+        }
+
+        try {
+            await stat(output);
+        } catch (ex) {
+            console.log(ex);
+            throw error.segment.Failed;
         }
 
         await PushApi.push('片段制作完成', `${clip.id},${clip.title}, ${filename}`);
