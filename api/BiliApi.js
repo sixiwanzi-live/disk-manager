bf888fa80f17b19263e31226e95b41c0c2535676
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-09-09 13:59:48 +0800

    dev

--- api/BiliApi.js
+++ api/BiliApi.js
@@ -18,22 +18,22 @@ export default class BiliApi {
         const res = await new Promise((res, rej) => {
             const playurl = `https://api.bilibili.com/x/player/playurl?bvid=${bv}e&cid=${cid}&qn=${qn}&fourk=1`;
             console.log(playurl);
-            const cmd = `curl "${playurl}" ^
-                -H "authority: api.bilibili.com" ^
-                -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" ^
-                -H "accept-language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6" ^
-                -H "cache-control: no-cache" ^
-                -H "cookie: ${config.segment.cookie}" ^
-                -H "pragma: no-cache" ^
-                -H "sec-ch-ua: ^\^"Microsoft Edge^\^";v=^\^"105^\^", ^\^" Not;A Brand^\^";v=^\^"99^\^", ^\^"Chromium^\^";v=^\^"105^\^"" ^
-                -H "sec-ch-ua-mobile: ?0" ^
-                -H "sec-ch-ua-platform: ^\^"Windows^\^"" ^
-                -H "sec-fetch-dest: document" ^
-                -H "sec-fetch-mode: navigate" ^
-                -H "sec-fetch-site: none" ^
-                -H "sec-fetch-user: ?1" ^
-                -H "upgrade-insecure-requests: 1" ^
-                -H "user-agent: ${config.segment.userAgent}" ^
+            const cmd = `curl '${playurl}' \
+                -H 'authority: api.bilibili.com' \
+                -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+                -H 'accept-language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6' \
+                -H 'cache-control: no-cache' \
+                -H 'cookie: ${config.segment.cookie}' \
+                -H 'pragma: no-cache' \
+                -H 'sec-ch-ua: "Microsoft Edge";v="105", " Not;A Brand";v="99", "Chromium";v="105"' \
+                -H 'sec-ch-ua-mobile: ?0' \
+                -H 'sec-ch-ua-platform: "Windows"' \
+                -H 'sec-fetch-dest: document' \
+                -H 'sec-fetch-mode: navigate' \
+                -H 'sec-fetch-site: none' \
+                -H 'sec-fetch-user: ?1' \
+                -H 'upgrade-insecure-requests: 1' \
+                -H 'user-agent: ${config.segment.userAgent}' \
                 --compressed`;
             exec(cmd, (error, stdout, stderr) => {
                 if (error) {
