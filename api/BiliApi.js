384406822ae3a85e8c47f0bfe598944dc8e91664
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-09-09 16:15:15 +0800

    dev

--- api/BiliApi.js
+++ api/BiliApi.js
@@ -31,7 +31,7 @@ export default class BiliApi {
                 -H 'sec-fetch-site: none' \
                 -H 'sec-fetch-user: ?1' \
                 -H 'upgrade-insecure-requests: 1' \
-                -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.27' \
+                -H 'user-agent: ${config.segment.userAgent}' \
                 --compressed`;
             exec(cmd, (error, stdout, stderr) => {
                 if (error) {
