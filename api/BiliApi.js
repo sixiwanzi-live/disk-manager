91d2181078ee90d86881ee51c8c975e141172b4d
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-09-10 11:31:13 +0800

    配置大会员

--- api/BiliApi.js
+++ api/BiliApi.js
@@ -4,7 +4,7 @@ import error from '../error.js';
 import config from '../config.js';
 
 export default class BiliApi {
-    
+
     static async fetchCid(bv) {
         try {
             const res = await axios.get(`http://api.bilibili.com/x/web-interface/view?bvid=${bv}`);
@@ -21,19 +21,19 @@ export default class BiliApi {
             console.log(playurl);
             const cmd = `curl '${playurl}' \
                 -H 'authority: api.bilibili.com' \
-                -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+                -H 'accept: application/json, text/javascript, */*; q=0.01' \
                 -H 'accept-language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6' \
                 -H 'cache-control: no-cache' \
                 -H 'cookie: ${config.segment.cookie}' \
+                -H 'origin: https://player.bilibili.com' \
                 -H 'pragma: no-cache' \
+                -H 'referer: https://player.bilibili.com/' \
                 -H 'sec-ch-ua: "Microsoft Edge";v="105", " Not;A Brand";v="99", "Chromium";v="105"' \
                 -H 'sec-ch-ua-mobile: ?0' \
                 -H 'sec-ch-ua-platform: "Windows"' \
-                -H 'sec-fetch-dest: document' \
-                -H 'sec-fetch-mode: navigate' \
-                -H 'sec-fetch-site: none' \
-                -H 'sec-fetch-user: ?1' \
-                -H 'upgrade-insecure-requests: 1' \
+                -H 'sec-fetch-dest: empty' \
+                -H 'sec-fetch-mode: cors' \
+                -H 'sec-fetch-site: same-site' \
                 -H 'user-agent: ${config.segment.userAgent}' \
                 --compressed`;
             exec(cmd, (error, stdout, stderr) => {
