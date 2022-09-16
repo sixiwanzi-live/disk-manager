7bd1e924ca7bfbf47525521a116fef8cbaa3c5c5
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-09-16 22:51:48 +0800

    dev

--- api/PushApi.js
+++ api/PushApi.js
@@ -5,11 +5,12 @@ import config from '../config.js';
 export default class PushApi {
     static async push(title, content) {
         const url = `https://api2.pushdeer.com/message/push?pushkey=${config.push.key}&text=${title}&desp=${content}`;
-        const res = await (await fetch(encodeURI(url))).json();
-        if (res.data.code !== 0) {
+        const res = await fetch(encodeURI(url));
+        const data = await res.json();
+        if (data.code !== 0) {
             throw {
                 code: error.push.Failed.code,
-                message: res.data.message
+                message: data.message
             }
         }
     }
